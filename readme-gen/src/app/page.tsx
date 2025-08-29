8036e4b4bf7aaa99231e5e74f698d748f71f8d1b
narvindev <narvincg@gmail.com>
2025-08-29 14:38:29 +0530

    only temp time set route for dashboard page

--- readme-gen/src/app/page.tsx
+++ readme-gen/src/app/page.tsx
@@ -1,8 +1,7 @@
 import Topbar from "./components/Topbar";
 import Footer from "./components/Footer";
+import Page from "./dashboard/page";
 import ContentSection from "./components/ContentSection";
-import Login from "./auth/Login";
-import Signup from "./auth/Signup";
 
 export default function Home() {
   return (
@@ -10,8 +9,7 @@ export default function Home() {
       {/* <Topbar /> */}
       {/* <ContentSection /> */}
       {/* <Footer /> */}
-      {/* <Login /> */}
-      <Signup />
+      <Page />
     </>
   );
 }
